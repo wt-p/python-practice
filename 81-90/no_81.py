4eaf3d137128145c472a48e5ae4bc754ede0e30a
wt-p <wtbtty9955@gmail.com>
2025-01-19 23:58:48 +0900

    No81~82を実施

---
+++ 81-90/no_81.py
@@ -0,0 +1,8 @@
+# 自分の回答
+l = [False, True, False, False, True]
+print(f'Falseの数 : {l.count(False)}')
+
+# 回答
+l = [False, True, False, False, True]
+c = len(l) - sum(l)
+print(f'Falseの数 : {c}')
---
+++ 81-90/no_82.py
@@ -0,0 +1,9 @@
+# 自分の回答
+l = [False, True, False, False, True]
+new_l = [int(v) for v in l]
+print(f'作成したリスト : {new_l}')
+
+# 回答
+l = [False, True, False, False, True]
+new_l = [int(b) for b in l]
+print(f'作成したリスト : {new_l}')
